c19fcacfdf8fc189ee20ea43dc1c71cb5b7247ca
Pnleal1976 <patricio_leal@hotmail.com>
2023-08-23 20:15:35 -0300

    Adicionando arquivos da aula 04

---
+++ src/main/java/next/scholl/cesar/springaula4/SpringAula4Application.java
@@ -0,0 +1,13 @@
+package next.scholl.cesar.springaula4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAula4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAula4Application.class, args);
+	}
+
+}
